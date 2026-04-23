bb0ffb49e352e096215d09ceb2ce422fa800a9a2
agahera-design <agahera@gmail.com>
2026-04-23 04:48:53 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const contadores = document.querySelectorAll('.contador');
+const seccionPruebaSocial = document.querySelector('.prueba-social');
+
+let contadorIniciado = false;
+
+const iniciarContadores = () => {
+    contadores.forEach(contador => {
+        const objetivo = +contador.getAttribute('data-target');
+        let actual = 0;
+        const incremento = objetivo / 80;
+
+        const actualizarContador = () => {
+            actual += incremento;
+
+            if (actual < objetivo) {
+                contador.textContent = Math.floor(actual);
+                requestAnimationFrame(actualizarContador);
+            } else {
+                contador.textContent = objetivo.toLocaleString();
+            }
+        };
+
+        actualizarContador();
+    });
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && !contadorIniciado) {
+            iniciarContadores();
+            contadorIniciado = true;
+        }
+    });
+}, {
+    threshold: 0.4
+});
+
+observer.observe(seccionPruebaSocial);
